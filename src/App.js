46d0219b7ffadf4a050b1a37e8775f3aa748f509
justkeepgoin <manage06@naver.com>
2023-09-02 14:22:29 +0900

    MenuItem conmponent setting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
+import MenuItem from "./MenuItem";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>배달 주문 프로젝트</p>
-      </header>
+      <MenuItem
+        name="햄버거"
+        price={6500}
+        quantity={1}
+        onAddToCart={(name, price, quantity) => {
+          // 추가하기 버튼 클릭 핸들러 로직 구현
+          console.log(
+            `${quantity}개의 ${name}이(가) ${price}원으로 장바구니에 추가되었습니다.`
+          );
+        }}
+      />
     </div>
   );
 }
