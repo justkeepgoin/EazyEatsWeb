81c15b06afcc3bde5cf8837249e65e393618d81b
justkeepgoin <manage06@naver.com>
2023-09-05 17:04:25 +0900

    - 장바구니 관련 컴포넌트 App 컴포넌트에 연결
    - CartItem 컴포넌트 App 컴포넌트에 연결
    - CartModal 컴포넌트 App 컴포넌트에 연결
    - CartButton 컴포넌트 App 컴포넌트에 연결

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-// App.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import MenuList from './MenuList';
+import CartButton from "./CartButton";
+import CartModal from "./CartModal";
 
 const App = () => {
   // 음식 메뉴 데이터
@@ -12,9 +13,42 @@ const App = () => {
     { id: 5, name: '음식5', price: 7000, quantity: 0 },
   ];
 
+  // 장바구니 아이템 상태 관련 함수
+  const [cartItems, setCartItems] = useState([]);
+
+  // 수량 업데이트 함수
+  const updateCartItemQuantity = (itemId, newQuantity) => {
+    // 장바구니 수량 업데이트
+    const updatedCartItems = cartItems.map((item) =>
+      item.id === itemId ? { ...item, quantity: newQuantity } : item
+    );
+    setCartItems(updatedCartItems);
+  };
+
+  // 장바구니 모달 열기/닫기 상태 관련 함수
+  const [isCartModalOpen, setIsCartModalOpen] = useState(false);
+
+  // 장바구니 모달 열기 함수
+  const openCartModal = () => {
+    setIsCartModalOpen(true);
+  };
+  // 장바구니 모달 닫기 함수
+  const closeCartModal = () => {
+    setIsCartModalOpen(false);
+  };
+  
   return (
     <div className="App">
+      <h1>Market jeongly</h1>
+      <CartButton openCartModal={openCartModal} />
       <MenuList menuData={menuData} />
+      {isCartModalOpen && ( // 모달 열기 상태일 때만 모달을 렌더링
+        <CartModal
+          cartItems={cartItems}
+          onUpdateCartItemQuantity={updateCartItemQuantity}
+          onClose={closeCartModal}
+        />
+      )}
     </div>
   );
 };
