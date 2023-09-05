a82ca3139271078f9ae16a315adf52cef02ba426
justkeepgoin <manage06@naver.com>
2023-09-05 18:11:10 +0900

    feat. QuantityControl 컴포넌트 재사용을 위한 수정
    - CartModal 내 수량 조절시 음수값도 설정되는 문제 발생
    -> QuantityControl 컴포넌트 재사용을 통한 MenuItem, CartModal 컴포넌트 내 수량 조절 버튼 기능 통일을 위한 QuantityControl 컴포넌트 수정

--- src/QuantityControl.jsx
+++ src/QuantityControl.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-const QuantityControl = ({ quantity, onIncrement, onDecrement }) => {
+const QuantityControl = ({ value, onUpdate }) => {
+  const handleDecrement = () => {
+    if (value > 1) { // 음수가 아닌 경우에만 감소
+      onUpdate(value - 1);
+    }
+  };
+
+  const handleIncrement = () => {
+    onUpdate(value + 1);
+  };
+
   return (
     <div className="quantity-control">
-      <button className="decrement-button" onClick={onDecrement}>
+      <button className="decrement-button" onClick={handleDecrement}>
         -
       </button>
-      <span className="quantity">{quantity}</span>
-      <button className="increment-button" onClick={onIncrement}>
+      <span className="quantity">{value}</span>
+      <button className="increment-button" onClick={handleIncrement}>
         +
       </button>
     </div>
