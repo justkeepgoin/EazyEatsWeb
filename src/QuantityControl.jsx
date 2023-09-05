6fc6abc464b4cb0b1254c8ced4e63e3129ca454b
justkeepgoin <manage06@naver.com>
2023-09-06 01:09:30 +0900

    feat. 수량 조절 버튼 css 스타일링
    - styled-components를 적용한 css스타일링

--- src/QuantityControl.jsx
+++ src/QuantityControl.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import styled from 'styled-components';
+
 
 const QuantityControl = ({ value, onUpdate }) => {
   const handleDecrement = () => {
@@ -12,16 +14,41 @@ const QuantityControl = ({ value, onUpdate }) => {
   };
 
   return (
-    <div className="quantity-control">
-      <button className="decrement-button" onClick={handleDecrement}>
+    <StyledQuantityControl className="quantity-control">
+      <button className="decrement-button quantity-button" onClick={handleDecrement}>
         -
       </button>
       <span className="quantity">{value}</span>
-      <button className="increment-button" onClick={handleIncrement}>
+      <button className="increment-button quantity-button" onClick={handleIncrement}>
         +
       </button>
-    </div>
+    </StyledQuantityControl>
   );
 };
+
+const StyledQuantityControl = styled.div`
+  span {
+    padding: 5px 10px;
+  }
+
+  .quantity-button {
+    padding: 5px 10px;
+    margin: 5px; 
+    cursor: pointer;
+
+  }
+
+  .decrement-button,
+  .increment-button {
+    background-color: #fff; 
+    border: 1px solid #D8BFD8;
+    color: black;
+
+    &:hover {
+      background-color: #E6E6FA;
+    }
+  }
+
+`;
 
 export default QuantityControl;
