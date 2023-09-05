069cff81c3caa734c87cc01167da024552884ac4
justkeepgoin <manage06@naver.com>
2023-09-06 01:10:34 +0900

    feat. App 컴포넌트 내 메뉴데이터 수정, css 스타일링
    - App 컴포넌트 내 음식 메뉴 데이터 수정
    - styled-components를 적용한 상단 메뉴바 css 스타일링

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import MenuList from './MenuList';
 import CartButton from "./CartButton";
 import CartModal from "./CartModal";
+import styled from 'styled-components';
 
 const App = () => {
   // 음식 메뉴 데이터
   const menuData = [
-    { id: 1, name: '음식1', price: 6500, quantity: 0 },
-    { id: 2, name: '음식2', price: 3000, quantity: 0 },
-    { id: 3, name: '음식3', price: 15000, quantity: 0 },
-    { id: 4, name: '음식4', price: 21000, quantity: 0 },
-    { id: 5, name: '음식5', price: 7000, quantity: 0 },
+    { id: 1, name: '크림치즈', price: 4200, quantity: 0 },
+    { id: 2, name: '짬뽕 밀키트', price: 12000, quantity: 0 },
+    { id: 3, name: '블렌드 원두', price: 15000, quantity: 0 },
+    { id: 4, name: '회냉면', price: 10800, quantity: 0 },
+    { id: 5, name: '통살치킨', price: 7900, quantity: 0 },
   ];
 
   // 장바구니 아이템 상태 관련 함수
@@ -51,8 +52,10 @@ const App = () => {
   
   return (
     <div className="App">
+      <Header>
       <h1>Market jeongly</h1>
       <CartButton openCartModal={openCartModal} />
+      </Header>
       <MenuList menuData={menuData} onAddToCart={addToCart} />
       {isCartModalOpen && ( // 모달 열기 상태일 때만 모달을 렌더링
         <CartModal
@@ -64,5 +67,15 @@ const App = () => {
     </div>
   );
 };
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 30px 0px 30px;
+  background-color: #27005D;
+  color: #FAFAFA;
+  font: italic 1rem "Lato", sans-serif;
+`;
 
 export default App;
