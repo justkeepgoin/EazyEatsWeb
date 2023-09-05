8aab7e2fbb51f39095412fd7e3233b4954ad9309
justkeepgoin <manage06@naver.com>
2023-09-06 01:08:23 +0900

    feat. 장바구니 버튼 css 스타일링
    - styled-components를 적용한 css 스타일링

--- src/CartButton.jsx
+++ src/CartButton.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const CartButton = ({ openCartModal }) => {
   return (
-    <button className="cart-button" onClick={openCartModal}>
+    <StyledCartButton className="cart-button" onClick={openCartModal}>
       장바구니
-    </button>
+    </StyledCartButton>
   );
 };
+
+const StyledCartButton = styled.button`
+  background-color: #fff;
+  color: #27005D;
+  padding: 15px 50px;
+  border: none;
+  border-radius: 15px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #E6E6FA;
+  }
+`;
 
 export default CartButton;
